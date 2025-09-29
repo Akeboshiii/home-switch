340ad1485c7efd49d11c8204cd2edbdba3fabf8e
akebb <johnkuraya491@gmail.com>
2025-09-30 03:37:20 +0800

    Update esp8266.ino
    for arduino easy reading of files

--- esp8266.ino
+++ esp8266.ino
@@ -2,16 +2,16 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h> 
 
-const char* ssid = "YOUR_WIFI_SSID"; //name ng wifi
-const char* password = "YOUR_WIFI_PASSWORD"; //pass niya
-const char* apiUrl = "https://home-web-switch.vercel.app/data";  // {"relay1":true,"relay2":false}
+const char* ssid = "YOUR_WIFI_SSID"; 
+const char* password = "YOUR_WIFI_PASSWORD"; 
+const char* apiUrl = "https://home-web-switch.vercel.app/data"; 
 
 unsigned long lastRequest = 0;
 const unsigned long interval = 10000;
 
 void setup() {
-  Serial.begin(9600); // Esp TX to Arduino RX
-  Serial.println("Init...")
+  Serial.begin(9600);
+  Serial.println("Init...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) delay(500);
 }
@@ -30,8 +30,13 @@ void loop() {
 
       if (httpCode == 200) {
         String payload = http.getString();
-        payload.replace("'", "\""); // para sa double quote
-        Serial.println(payload);
+
+        bool relay1 = payload.indexOf("\"relay1\":true") > 0;
+        bool relay2 = payload.indexOf("\"relay2\":true") > 0;
+
+        Serial.print(relay1 ? "true" : "false");
+        Serial.print(",");
+        Serial.println(relay2 ? "true" : "false");
       }
 
       http.end();
