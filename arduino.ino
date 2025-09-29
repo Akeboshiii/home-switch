6829169c9bb73519ccd7c1ac48f5190073c21084
akebb <johnkuraya491@gmail.com>
2025-09-30 03:36:47 +0800

    Update arduino.ino
    for easy reading of boolean values

--- arduino.ino
+++ arduino.ino
@@ -1,5 +1,3 @@
-#include <ArduinoJson.h>
-
 const int relay1Pin = 2;
 const int relay2Pin = 3;
 
@@ -11,12 +9,17 @@ void setup() {
 
 void loop() {
   if (Serial.available()) {
-    String json = Serial.readStringUntil('\n');
+    String data = Serial.readStringUntil('\n');  // e.g. "true,false"
 
-    StaticJsonDocument<128> doc;
-    if (deserializeJson(doc, json) == DeserializationError::Ok) {
-      bool relay1State = doc["relay1"];
-      bool relay2State = doc["relay2"];
+    data.trim(); // Remove spaces or \r
+
+    int commaIndex = data.indexOf(',');
+    if (commaIndex > 0) {
+      String r1 = data.substring(0, commaIndex);        // before comma
+      String r2 = data.substring(commaIndex + 1);       // after comma
+
+      bool relay1State = (r1 == "true");
+      bool relay2State = (r2 == "true");
 
       digitalWrite(relay1Pin, relay1State);
       digitalWrite(relay2Pin, relay2State);
